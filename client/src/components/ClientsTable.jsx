3ec5ed19108824257efd9dced2c46ef5f3313cc4
Miry Konishi <miry.konishi@gmail.com>
2023-03-19 17:21:36 -0300

    Create client input created

---
+++ client/src/components/ClientsTable.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { DataGrid } from '@mui/x-data-grid';
+
+const ClientsTable = () => {
+  return (
+    <div>ClientsTable</div>
+  )
+}
+
+export default ClientsTable
--- client/src/pages/AdminDashboard.jsx
+++ client/src/pages/AdminDashboard.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import {Container, Typography, Box} from '@mui/material'
+import ClientCreateInputs from '../components/ClientCreateInputs'
+import ClientsTable from '../components/ClientsTable'
 
 const AdminDashboard = () => {
   return (
-    <div>AdminDashboard</div>
+    <Box
+      component="main"
+      sx={{ padding: "2%"}}
+    >
+        <Typography variant="h4" gutterBottom align="center">
+          Dashboard Admin
+        </Typography>
+        <ClientCreateInputs />
+        <ClientsTable />
+    </Box>
   )
 }
 
