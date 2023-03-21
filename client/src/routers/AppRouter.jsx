65816cdac8801553465446feaa3c193f5af91c44
Miry Konishi <miry.konishi@gmail.com>
2023-03-21 17:14:16 -0300

    Client login page UI done

--- client/src/routers/AppRouter.jsx
+++ client/src/routers/AppRouter.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AdminDashboard from "../pages/AdminDashboard";
 import AdminLogin from "../pages/AdminLogin";
+import ClientDashboard from "../pages/ClientDashboard";
+import ClientLogin from "../pages/ClientLogin";
+import ClientRead from "../pages/ClientRead";
 
 const AppRouter = () => {
   return (
@@ -9,6 +12,9 @@ const AppRouter = () => {
       <Routes>
         <Route path='/admin' element={<AdminLogin/>}/>
         <Route path='/admin/home' element={<AdminDashboard/>}/>
+        <Route path='/' element={<ClientLogin />} exact/>
+        <Route path='/home' element={<ClientDashboard />}/>
+        <Route path='/read' element={<ClientRead />}/>
       </Routes>
     </BrowserRouter>
   );
