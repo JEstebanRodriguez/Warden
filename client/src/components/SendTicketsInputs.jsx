a37e15fc5a65f5051080b29f1c5578cfc50a302c
XJDEVX <xjergx.dev@gmail.com>
2023-03-22 16:38:22 -0300

    Ticket front create stash 1

--- client/src/components/SendTicketsInputs.jsx
+++ client/src/components/SendTicketsInputs.jsx
@@ -1,107 +1,130 @@
-import React from "react";
-import { useForm } from "react-hook-form";
-import { Paper, TextField, Button, Box, Divider, Typography } from "@mui/material";
+import React, { useContext } from 'react'
+import { useForm } from 'react-hook-form'
+import {
+	Paper,
+	TextField,
+	Button,
+	Box,
+	Divider,
+	Typography
+} from '@mui/material'
+import { UserContext } from '../context/UserContext'
+import { ApiURL } from '../main'
+import { toast } from 'react-hot-toast'
 
 const SendTicketsInputs = ({ set, logs }) => {
-  const initialValues = {
-    receiverEmail: "",
-    quantity: 1,
-  };
+	const { user } = useContext(UserContext)
+	const initialValues = {
+		receiverEmail: '',
+		quantity: 1
+	}
 
-  const {
-    register,
-    handleSubmit,
-    formState,
-    formState: { errors, isSubmitSuccessfull, isValid },
-    reset,
-  } = useForm({
-    mode: "onChange",
-    defaultValues: initialValues,
-  });
+	const {
+		register,
+		handleSubmit,
+		formState,
+		formState: { errors, isSubmitSuccessfull, isValid },
+		reset
+	} = useForm({
+		mode: 'onChange',
+		defaultValues: initialValues
+	})
 
-  const onSubmit = (data) => {
-    console.log(data);
-    let aux = [...logs];
-    // set new Logs state with de new data
-    aux.push(data);
-    set(aux);
+	const onSubmit = async (values) => {
+		const body = {
+			email: values.receiverEmail,
+			qty: +values.quantity,
+			event_id: user._id
+		}
+		try {
+			const { data: ticket } = await ApiURL.post('/tickets/add', body)
+			toast.success(ticket.data)
+		} catch (err) {
+			console.error(err)
+		}
 
-    // Reset Inputs
-    if (isSubmitSuccessfull) {
-      reset(initialValues);
-    }
-  };
+		let aux = [...logs]
+		// set new Logs state with de new values
+		aux.push(values)
+		set(aux)
 
-  return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <Paper
-        sx={{
-          width: "100%",
-          height: "145px",
-          padding: "20px",
-        }}
-      >
-        <Divider sx={{marginBottom: '10px'}}><Typography variant='button' display="block">Complete the form and send your tickets</Typography></Divider>
-        <Box
-          sx={{
-            display: "flex",
-            gap: "10px",
-            justifyContent: "space-around",
-            alignItems: "top",
-          }}
-        >
-          <TextField
-            {...register("receiverEmail", {
-              required: "An email is required",
-              pattern: {
-                value: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g,
-                message: "Please enter a valid mail",
-              },
-            })}
-            type="email"
-            id="receiverEmail"
-            name="receiverEmail"
-            label="Email to send the tickets"
-            variant="outlined"
-            sx={{ flex: 5 }}
-            error={errors.receiverEmail ? true : false}
-            helperText={errors.receiverEmail?.message}
-          />
-          <TextField
-            {...register("quantity", {
-              required: "Quantity is required",
-              min: {
-                value: 1,
-                message:
-                  "Minimun of tickets is 1. Zero or negative numbers are not accepted",
-              },
-            })}
-            name="quantity"
-            id="quantity"
-            type="number"
-            InputProps={{ inputProps: { min: 1 } }}
-            min={1}
-            label="Quantity of tickets to send"
-            error={errors.quantity ? true : false}
-            helperText={errors.quantity?.message}
-            variant="outlined"
-            sx={{ flex: 3 }}
-          />
-          <Button
-            type="submit"
-            variant="contained"
-            color="info"
-            disableElevation
-            size="medium"
-            sx={{ height: "max-content", flex: "1", marginTop: "10px" }}
-            disabled={!isValid}
-          >
-            Send Ticket/s
-          </Button>
-        </Box>
-      </Paper>
-    </form>
-  );
-};
+		// Reset Inputs
+		if (isSubmitSuccessfull) {
+			reset(initialValues)
+		}
+	}
 
-export default SendTicketsInputs;
+	return (
+		<form onSubmit={handleSubmit(onSubmit)}>
+			<Paper
+				sx={{
+					width: '100%',
+					height: '145px',
+					padding: '20px'
+				}}>
+				<Divider sx={{ marginBottom: '10px' }}>
+					<Typography variant='button' display='block'>
+						Complete the form and send your tickets
+					</Typography>
+				</Divider>
+				<Box
+					sx={{
+						display: 'flex',
+						gap: '10px',
+						justifyContent: 'space-around',
+						alignItems: 'top'
+					}}>
+					<TextField
+						{...register('receiverEmail', {
+							required: 'An email is required',
+							pattern: {
+								value: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g,
+								message: 'Please enter a valid mail'
+							}
+						})}
+						type='email'
+						id='receiverEmail'
+						name='receiverEmail'
+						label='Email to send the tickets'
+						variant='outlined'
+						sx={{ flex: 5 }}
+						error={errors.receiverEmail ? true : false}
+						helperText={errors.receiverEmail?.message}
+					/>
+					<TextField
+						{...register('quantity', {
+							required: 'Quantity is required',
+							min: {
+								value: 1,
+								message:
+									'Minimun of tickets is 1. Zero or negative numbers are not accepted'
+							}
+						})}
+						name='quantity'
+						id='quantity'
+						type='number'
+						InputProps={{ inputProps: { min: 1 } }}
+						min={1}
+						label='Quantity of tickets to send'
+						error={errors.quantity ? true : false}
+						helperText={errors.quantity?.message}
+						variant='outlined'
+						sx={{ flex: 3 }}
+					/>
+					<Button
+						type='submit'
+						variant='contained'
+						color='info'
+						disableElevation
+						size='medium'
+						sx={{ height: 'max-content', flex: '1', marginTop: '10px' }}
+						disabled={!isValid}>
+						Send Ticket/s
+					</Button>
+				</Box>
+			</Paper>
+		</form>
+	)
+}
+
+export default SendTicketsInputs
