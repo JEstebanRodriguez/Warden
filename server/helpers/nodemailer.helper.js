aa6b01f49a32053a15d5ea65084af1ef2fef229e
Brihan Penayo <brihan.penayos@gmail.com>
2023-03-22 10:28:38 -0300

    Fixing nodemailer helper

---
+++ server/helpers/nodemailer.helper.js
@@ -0,0 +1,24 @@
+import nodemailer from 'nodemailer'
+// import dotenv from 'dotenv'
+// dotenv.config();
+
+export const notifyAdmin = async () => {
+    const tp = nodemailer.createTransport({
+        host: "in-v3.mailjet.com",
+        port: 465,
+        secure: true,
+        auth: {
+            user: process.env.SMTP_ID,
+            pass: process.env.SMTP_PS
+        }
+    });
+
+    const data = await tp.sendMail({
+        from: `Warder Inc. <${process.env.GGL_ACC}>`,
+        to: "mail@ejemplo.com",
+        subject: "New Ticket was Created",
+        html: "<h1>Hola World</h1>"
+    })
+
+    return data;
+}
