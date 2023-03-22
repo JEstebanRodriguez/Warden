dfc4be53c99e0daf07a08800b7d4f6ed0b01a8f4
XJDEVX <xjergx.dev@gmail.com>
2023-03-22 13:32:47 -0300

    Add Logout in ClientDashboard

--- client/src/pages/ClientDashboard.jsx
+++ client/src/pages/ClientDashboard.jsx
@@ -3,36 +3,55 @@ import { Container, Typography, Box, Fab } from "@mui/material";
 import LogoutIcon from "@mui/icons-material/Logout";
 import SendTicketsInputs from "../components/SendTicketsInputs";
 import SentTicketsTable from "../components/SentTicketsTable";
+import { useNavigate } from 'react-router-dom'
 
 const ClientDashboard = () => {
-  const [logs, setLogs] = useState([]);
+	const [logs, setLogs] = useState([])
+	const navigate = useNavigate()
 
-  return (
-    <Box component="main" sx={{ padding: "1%" }}>
-      <Box sx={{ display: "flex", alignItems: "center", justifyContent:'end', marginBottom: '10px' }}>
-        <Typography
-          variant="h4"
-          align="center"
-          sx={{ flex: 3, textTransform: "uppercase", fontWeight: "bold", marginLeft: '30px' }}
-        >
-          Welcome to your dashboard
-        </Typography>
-        <Box>
-          <Fab
-            variant="extended"
-            size="medium"
-            color="secondary"
-            sx={{ flex: 1 }}
-          >
-            <LogoutIcon />
-            Logout
-          </Fab>
-        </Box>
-      </Box>
-      <SendTicketsInputs logs={logs} set={setLogs}/> {/* have a function that refreshes the state of register when new mail is sent*/}
-      <SentTicketsTable logs={logs} />
-    </Box>
-  );
-};
+	const handleLogout = () => {
+		localStorage.removeItem('user')
+		localStorage.removeItem('token')
+		navigate('/')
+	}
+
+	return (
+		<Box component='main' sx={{ padding: '1%' }}>
+			<Box
+				sx={{
+					display: 'flex',
+					alignItems: 'center',
+					justifyContent: 'end',
+					marginBottom: '10px'
+				}}>
+				<Typography
+					variant='h4'
+					align='center'
+					sx={{
+						flex: 3,
+						textTransform: 'uppercase',
+						fontWeight: 'bold',
+						marginLeft: '30px'
+					}}>
+					Welcome to your dashboard
+				</Typography>
+				<Box>
+					<Fab
+						onClick={handleLogout}
+						variant='extended'
+						size='medium'
+						color='secondary'
+						sx={{ flex: 1 }}>
+						<LogoutIcon />
+						Logout
+					</Fab>
+				</Box>
+			</Box>
+			<SendTicketsInputs logs={logs} set={setLogs} />{' '}
+			{/* have a function that refreshes the state of register when new mail is sent*/}
+			<SentTicketsTable logs={logs} />
+		</Box>
+	)
+}
 
 export default ClientDashboard;
