d9efc0f18387b649254260b16d819e0ba249e6d1
XJDEVX <xjergx.dev@gmail.com>
2023-03-22 16:46:53 -0300

    Ticket front create stash 2

--- client/src/components/SendTicketsInputs.jsx
+++ client/src/components/SendTicketsInputs.jsx
@@ -10,7 +10,7 @@ import {
 } from '@mui/material'
 import { UserContext } from '../context/UserContext'
 import { ApiURL } from '../main'
-import { toast } from 'react-hot-toast'
+import { toast, Toaster } from 'react-hot-toast'
 
 const SendTicketsInputs = ({ set, logs }) => {
 	const { user } = useContext(UserContext)
@@ -39,6 +39,7 @@ const SendTicketsInputs = ({ set, logs }) => {
 		try {
 			const { data: ticket } = await ApiURL.post('/tickets/add', body)
 			toast.success(ticket.data)
+			console.log(ticket)
 		} catch (err) {
 			console.error(err)
 		}
@@ -55,75 +56,78 @@ const SendTicketsInputs = ({ set, logs }) => {
 	}
 
 	return (
-		<form onSubmit={handleSubmit(onSubmit)}>
-			<Paper
-				sx={{
-					width: '100%',
-					height: '145px',
-					padding: '20px'
-				}}>
-				<Divider sx={{ marginBottom: '10px' }}>
-					<Typography variant='button' display='block'>
-						Complete the form and send your tickets
-					</Typography>
-				</Divider>
-				<Box
+		<>
+			<Toaster position='top-center' />
+			<form onSubmit={handleSubmit(onSubmit)}>
+				<Paper
 					sx={{
-						display: 'flex',
-						gap: '10px',
-						justifyContent: 'space-around',
-						alignItems: 'top'
+						width: '100%',
+						height: '145px',
+						padding: '20px'
 					}}>
-					<TextField
-						{...register('receiverEmail', {
-							required: 'An email is required',
-							pattern: {
-								value: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g,
-								message: 'Please enter a valid mail'
-							}
-						})}
-						type='email'
-						id='receiverEmail'
-						name='receiverEmail'
-						label='Email to send the tickets'
-						variant='outlined'
-						sx={{ flex: 5 }}
-						error={errors.receiverEmail ? true : false}
-						helperText={errors.receiverEmail?.message}
-					/>
-					<TextField
-						{...register('quantity', {
-							required: 'Quantity is required',
-							min: {
-								value: 1,
-								message:
-									'Minimun of tickets is 1. Zero or negative numbers are not accepted'
-							}
-						})}
-						name='quantity'
-						id='quantity'
-						type='number'
-						InputProps={{ inputProps: { min: 1 } }}
-						min={1}
-						label='Quantity of tickets to send'
-						error={errors.quantity ? true : false}
-						helperText={errors.quantity?.message}
-						variant='outlined'
-						sx={{ flex: 3 }}
-					/>
-					<Button
-						type='submit'
-						variant='contained'
-						color='info'
-						disableElevation
-						size='medium'
-						sx={{ height: 'max-content', flex: '1', marginTop: '10px' }}
-						disabled={!isValid}>
-						Send Ticket/s
-					</Button>
-				</Box>
-			</Paper>
-		</form>
+					<Divider sx={{ marginBottom: '10px' }}>
+						<Typography variant='button' display='block'>
+							Complete the form and send your tickets
+						</Typography>
+					</Divider>
+					<Box
+						sx={{
+							display: 'flex',
+							gap: '10px',
+							justifyContent: 'space-around',
+							alignItems: 'top'
+						}}>
+						<TextField
+							{...register('receiverEmail', {
+								required: 'An email is required',
+								pattern: {
+									value: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g,
+									message: 'Please enter a valid mail'
+								}
+							})}
+							type='email'
+							id='receiverEmail'
+							name='receiverEmail'
+							label='Email to send the tickets'
+							variant='outlined'
+							sx={{ flex: 5 }}
+							error={errors.receiverEmail ? true : false}
+							helperText={errors.receiverEmail?.message}
+						/>
+						<TextField
+							{...register('quantity', {
+								required: 'Quantity is required',
+								min: {
+									value: 1,
+									message:
+										'Minimun of tickets is 1. Zero or negative numbers are not accepted'
+								}
+							})}
+							name='quantity'
+							id='quantity'
+							type='number'
+							InputProps={{ inputProps: { min: 1 } }}
+							min={1}
+							label='Quantity of tickets to send'
+							error={errors.quantity ? true : false}
+							helperText={errors.quantity?.message}
+							variant='outlined'
+							sx={{ flex: 3 }}
+						/>
+						<Button
+							type='submit'
+							variant='contained'
+							color='info'
+							disableElevation
+							size='medium'
+							sx={{ height: 'max-content', flex: '1', marginTop: '10px' }}
+							disabled={!isValid}>
+							Send Ticket/s
+						</Button>
+					</Box>
+				</Paper>
+			</form>
+		</>
 	)
 }
 
