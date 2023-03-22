e6efc2a3052330174bb185b3d347ee728d58179b
Miry K <115422647+miryk@users.noreply.github.com>
2023-03-21 23:39:46 -0300

    Client dashboard UI (#13)

---
+++ client/src/components/SentTicketsTable.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { Box, Paper, IconButton, Typography } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import TableTitle from "./TableTitle";
+
+const SentTicketsTable = ({ logs }) => {
+  // Dummy data for testing
+  const rows = [
+    {id: 1, receiverEmail: 'ma@mail.com', quantity: 10},
+    {id: 2, receiverEmail: 'ma@mail.com', quantity: 15},
+    {id: 3, receiverEmail: 'ma@mail.com', quantity: 20},
+    {id: 4, receiverEmail: 'ma@mail.com', quantity: 25}
+  ]
+
+  const columns = [
+    // {field: 'id', headerName: 'ID', flex: 1},
+    { field: "receiverEmail", headerName: "Email of Receiver", flex: 3 },
+    { field: "quantity", headerName: "Quantity of Tickets", flex: 2 },
+    {field: 'actions', headerName: 'Actions', flex: 2,
+  renderCell: (params) => (
+    <>
+    <IconButton
+      // variant='contained'
+      // onClick={deleteTickets(eventID)}  // function to delete all tickets to this receiver
+      aria-label="delete"
+      color='error'
+    >
+      <DeleteForeverIcon />
+
+    </IconButton>
+    </>
+  )
+  }
+  ];
+
+  return (
+    <Box sx={{ marginTop: "20px" }}>
+      <Paper sx={{ padding: "20px", height: 700 }}>
+        <DataGrid 
+        // getRowId={(row) => row._id}  To grab the unique identifier from property _id
+        rows={logs} columns={columns} checkboxSelection slots={{
+          toolbar: TableTitle
+        }} />
+      </Paper>
+    </Box>
+  );
+};
+
+export default SentTicketsTable;
--- client/src/pages/ClientDashboard.jsx
+++ client/src/pages/ClientDashboard.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Container, Typography, Box, Fab } from "@mui/material";
+import LogoutIcon from "@mui/icons-material/Logout";
+import SendTicketsInputs from "../components/SendTicketsInputs";
+import SentTicketsTable from "../components/SentTicketsTable";
 
 const ClientDashboard = () => {
-  return (
-    <div>ClientDashboard</div>
-  )
-}
+  const [logs, setLogs] = useState([]);
 
-export default ClientDashboard
+  return (
+    <Box component="main" sx={{ padding: "1%" }}>
+      <Box sx={{ display: "flex", alignItems: "center", justifyContent:'end', marginBottom: '10px' }}>
+        <Typography
+          variant="h4"
+          align="center"
+          sx={{ flex: 3, textTransform: "uppercase", fontWeight: "bold", marginLeft: '30px' }}
+        >
+          Welcome to your dashboard
+        </Typography>
+        <Box>
+          <Fab
+            variant="extended"
+            size="medium"
+            color="secondary"
+            sx={{ flex: 1 }}
+          >
+            <LogoutIcon />
+            Logout
+          </Fab>
+        </Box>
+      </Box>
+      <SendTicketsInputs logs={logs} set={setLogs}/> {/* have a function that refreshes the state of register when new mail is sent*/}
+      <SentTicketsTable logs={logs} />
+    </Box>
+  );
+};
+
+export default ClientDashboard;
