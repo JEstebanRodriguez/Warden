bc53f00b4ae5e9666eed057565baa6a80c4624e8
Brihan Penayo <brihan.penayos@gmail.com>
2023-04-14 18:04:05 -0400

    Modificando valores .env y agregando .env-copy

--- client/src/pages/ClientDashboard.jsx
+++ client/src/pages/ClientDashboard.jsx
@@ -5,6 +5,7 @@ import SendTicketsInputs from '../components/SendTicketsInputs'
 import SentTicketsTable from '../components/SentTicketsTable'
 import { useNavigate } from 'react-router-dom'
 import { UserContext } from '../context/UserContext'
+import QrCodeScannerIcon from '@mui/icons-material/QrCodeScanner';
 
 const ClientDashboard = () => {
 	const [logs, setLogs] = useState([])
@@ -71,6 +72,13 @@ const ClientDashboard = () => {
 						Logout
 					</Fab>
 				</Box>
+				<Fab
+					onClick={() => window.open("http://warden.coding-py.com/read", "_blank")}
+					size='large'
+					color='primary'
+					sx={{ position: "absolute", right: 30, bottom: 30, width: 75, height: 75, zIndex: 99999 }}>
+					<QrCodeScannerIcon sx={{ width: "55%", height: "55%" }} />
+				</Fab>
 			</Box>
 			<SendTicketsInputs logs={logs} set={setLogs} />
 			{/* have a function that refreshes the state of register when new mail is sent*/}
